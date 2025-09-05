39a4b60e00a7bef99aff95f2124bd722dbc97395
DarshikaMetropolia25 <darshikasanjeewani57@gmail.com>
2025-09-06 02:55:46 +0530

    I have amended task01 and task04 from module02

--- Module02/task04.py
+++ Module02/task04.py
@@ -1,4 +1,4 @@
 A=int(input("Enter the number01 = "))
 B=int(input("Enter the number02 = "))
 C=int(input("Enter the number03 = "))
-print("Sum=", A+B+C , "\nProduct=", A*B*C , "\nAverage=",A+B+C/3)
+print("Sum=", A+B+C , "\nProduct=", A*B*C , "\nAverage=",(A+B+C)/3)
--- Module03/task01.py
+++ Module03/task01.py
@@ -2,4 +2,5 @@ LnFishCt = float(input("Enter the length of zander in cm = "))
 if LnFishCt < 42:
     length= 42-LnFishCt
     print("Release tha zander back to the lake, \n Your fish length is " , length, "cm below the size limit")
-
+else:
+    print("You can keep the fish")
