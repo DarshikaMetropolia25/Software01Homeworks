faade1ba805a64981a88e9545ae44b67d03d50be
DarshikaMetropolia25 <darshikasanjeewani57@gmail.com>
2025-11-17 23:47:20 +0200

    I have completed all tasks from module 13

--- Module13/Airports.py
+++ Module13/Airports.py
@@ -1,5 +1,41 @@
-from flask import Flask
+from flask import Flask, jsonify
+import mysql.connector
 
 app = Flask(__name__)
 
-@app.route('/prime_number/<number1>')
+connection = mysql.connector.connect(
+    host='127.0.0.1',
+    port=3306,
+    database='flight_game',
+    user='userdarshika',
+    password='ADIL2022',
+    autocommit=True
+)
+
+@app.route('/airport/<icao_code>')
+def get_airport_Name_and_Location(icao_code):
+    sql = (
+        f"SELECT airport.name, airport.municipality "
+        f"FROM airport WHERE airport.ident='{icao_code}'"
+    )
+
+    cursor = connection.cursor()
+    cursor.execute(sql)
+    result = cursor.fetchall()
+
+    if cursor.rowcount > 0:
+        name = result[0][0]
+        location = result[0][1]
+        response = {
+            "ICAO": icao_code,
+            "Name": name,
+            "Location": location
+        }
+    else:
+        response = {"error": "Airport not found"}
+
+    return jsonify(response)
+
+
+if __name__ == '__main__':
+    app.run(use_reloader=True, host='127.0.0.1', port=3000)
